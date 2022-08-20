281609d2d6cbebdb8228c5997b5e9f6616bee5d2
abdur-rehman-ali <alijoyia911@gmail.com>
2022-08-20 22:56:11 +0500

    request feature implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
     def index
         @users = User.all 
     end
+
+    def trade
+        @request = TradeRequest.new(user_id:params[:user_id],sent_by:params[:sent_by])
+        if @request.save
+            redirect_to users_path, notice: 'Request sent'
+        else
+            redirect_to users_path, alert: 'Request sent failed'
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index]
   devise_for :users
+  post '/trade', to: 'users#trade'
   root to: 'home#index'
 end
