be25f6c6c99ec169bc605e7d311c620236eedb30
abdur-rehman-ali <alijoyia911@gmail.com>
2022-08-20 19:27:36 +0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+    User.create(
+        email: "user#{i+1}@gmail.com",
+        password: 'dev1234',
+        password_confirmation: 'dev1234'
+    )
+end
